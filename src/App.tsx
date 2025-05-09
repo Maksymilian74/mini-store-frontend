03c60cf316da45a9224399f642d09c19fa2383ed
Maksymilian74 <273007@student.pwr.edu.pl>
2025-05-10 00:45:19 +0200

    Apply custom MUI theme with palette and typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {AppRoutes} from './routes/Router.tsx';
+import {ThemeProvider} from '@mui/material';
+import theme from './theme/theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <AppRoutes />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/typography.ts
@@ -0,0 +1,17 @@
+const typography = {
+  fontFamily: "'Inter', sans-serif",
+  h1: {
+    fontSize: '2.5rem',
+    fontWeight: 600,
+  },
+  body1: {
+    fontSize: '1rem',
+    fontWeight: 400,
+  },
+  body2: {
+    fontSize: '0.8rem',
+    fontWeight: 400,
+  },
+};
+
+export default typography;
