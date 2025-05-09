163ef4a1556b1a9c6d83439d4edf3ae546a31454
Maksymilian74 <273007@student.pwr.edu.pl>
2025-05-09 20:29:41 +0200

    Fix ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {ignores: ['dist']},
+  prettier,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [
-        js.configs.recommended,
-        ...tseslint.configs.recommended,
-        'eslint-config-prettier'
-    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -25,8 +24,8 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        {allowConstantExport: true},
       ],
     },
-  },
-)
+  }
+);
