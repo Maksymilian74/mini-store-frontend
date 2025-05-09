971dc042c319516d8bb2254e533cd9984dd1ae3c
Maksymilian74 <273007@student.pwr.edu.pl>
2025-05-09 20:36:07 +0200

    Format code with Prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 export default {
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'es5',
-    bracketSpacing: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: false,
 };
